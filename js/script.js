5445bcdf7aa5d2877ccc560570d57cf3188c3bbf
axowatl <117409439+axowatl@users.noreply.github.com>
2025-09-26 09:58:08 -0500

    Push2

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/**
+ * @type {HTMLIFrameElement}
+ */
+const frame = document.getElementById('frame');
+frame.src = "https://open.spotify.com/embed/track/5mfEcAbAcufmTpMQJrq6K5?utm_source=generator";
